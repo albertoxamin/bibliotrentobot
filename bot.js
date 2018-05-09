da0623e7c960e0289eb4b28b8558bf6538c4f1ce
Alberto Xamin <alberto@xamin.it>
2018-05-09 12:09:47 +0200

    add cache add /help /start

--- bot.js
+++ bot.js
@@ -1,6 +1,7 @@
 const Telegraf = require('telegraf');
 const request = require('request');
 const crypto = require('crypto');
+const moment = require('moment');
 var config;
 try { config = require('./config') } catch (err) {
     config = {
@@ -15,23 +16,39 @@ bot.telegram.getMe().then((bot_informations) => {
     console.log("Server has initialized bot nickname. Nick: " + bot_informations.username);
 });
 
+bot.command(['help', 'start'], ctx => {
+    ctx.replyWithMarkdown('Benvenuto a bibliotrentobot.\nQuesto bot ti permette di consultare gli orari delle biblioteche universitarie di Trento\n\nScrivimi qualsiasi cosa e ti risponderò con gli orari di **oggi**\n\nIn caso di problemi con il bot contattate @albertoxamin\n\nContribuisci allo sviluppo su https://github.com/albertoxamin/bibliotrentobot\nOppure puoi offrirmi un caffè http://buymeacoff.ee/Xamin');
+});
+
+var lastUnix = "";
+var cachedMessage = "";
+
 const getBiblio = function (callback) {
-    request('http://spotted-biblio.herokuapp.com/raw', (err, response, body) => {
-        if (err || response.statusCode != 200)
-            return "Errore di connessione";
-        body = JSON.parse(body);
-        delete (body.lastUpdate);
-        let message = "";
-        for (let property in body) {
-            if (body.hasOwnProperty(property)) {
-                message += `*${property}* \n\t🔓 \`${body[property].open}\` 🔐 \`${body[property].close}\`\n`;
+    let m = moment().utcOffset(0);
+    m.set({ hour: 0, minute: 0, second: 0, millisecond: 0 })
+    let todayUnix = m.unix().toString() + "000";
+    if (todayUnix != lastUnix) {
+        lastUnix = lastUnix;
+        request('http://spotted-biblio.herokuapp.com/raw', (err, response, body) => {
+            if (err || response.statusCode != 200)
+                return "Errore di connessione";
+            body = JSON.parse(body);
+            delete (body.lastUpdate);
+            let message = "";
+            for (let property in body) {
+                if (body.hasOwnProperty(property)) {
+                    message += `*${property}* \n\t🔓 \`${body[property].open}\` 🔐 \`${body[property].close}\`\n`;
+                }
             }
-        }
-        callback(message);
-    });
+            cachedMessage = message;
+            callback(message);
+        });
+    } else {
+        callback(cachedMessage);
+    }
 }
 
-bot.on('text', ctx =>{
+bot.on('text', ctx => {
     getBiblio(res => ctx.replyWithMarkdown(res));
 });
 
@@ -41,7 +58,7 @@ bot.on('inline_query', async ({ inlineQuery, answerInlineQuery }) => {
             type: 'article',
             id: crypto.createHash('md5').update(res).digest('hex'),
             title: "Orari di oggi",
-            description : res,
+            description: res,
             input_message_content: {
                 message_text: res,
                 parse_mode: 'Markdown'
